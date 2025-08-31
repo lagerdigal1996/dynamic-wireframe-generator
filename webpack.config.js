9841e1658b1e43f8a31cd519f4abc895640c61da
lagerdigal1996 <kpogohhymes@hotmail.com>
2025-08-31 03:38:56 -0500

    feat: initial commit for webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,  // This rule applies to .js files
+        exclude: /node_modules/,  // Exclude the node_modules folder
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,  // This rule applies to .css files
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
